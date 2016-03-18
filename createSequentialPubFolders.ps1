ce4ced54e85c10915392203d32feb6a1a30ff16d
Caleb Angelino <belac9615@gmail.com>
2016-03-18 09:13:17 -0400

    Added Sequential Public Folders script

---
+++ createSequentialPubFolders.ps1
@@ -0,0 +1,23 @@
+﻿# This script is used to create sequentially numbered Public Folders on Exchange
+# It has been tested with Exchange 2010
+
+$Path = read-host "Please enter the path with a trailing \"
+# If you want to script your path, comment out the above line and modify below
+# $Path = "\Your Public Folder\"
+
+[int]$StartingFolder = read-host "Please enter the starting number"
+[int]$EndingFolder = read-host "Please enter the ending number"
+
+If (-not $StartingFolder){
+    Write-Output "You must enter a number."
+    Exit
+}
+If ($EndingFolder -le $StartingFolder){
+    Write-Output “The ending folder number must be larger than the starting folder number.”
+    Exit
+}
+for ($i=$StartingFolder ; $i -le $EndingFolder; $i++) {
+    New-PublicFolder -Name $i -Path $Path
+    $pubFolder = $Path + $i + "\"
+    Enable-MailPublicFolder -Identity $pubFolder
+}
